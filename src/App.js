be275bbaa34003c4332009c2a265cee458e19d8b
sakisalman <codersaki98@gmail.com>
2022-07-05 08:42:03 -0700

    uploaded

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+import Page4 from './components/Page4';
+
+function App() {
+
+  return (
+    <>
+    <Page1></Page1>
+    <Page2></Page2>
+    <Page3></Page3>
+    <Page4></Page4>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Page1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page1 = () => {
+
+  return (
+    <div className="my-2">
+      <input className='form-control'  type="text" />
+      <h1></h1>
+    </div>
+  )
+}
+
+export default Page1
